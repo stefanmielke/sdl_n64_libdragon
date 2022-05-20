64db41732366dce7b4ec6efaf10cc6c629cdedee
Stefan Mielke <stefanmielke@fastmail.com>
2022-05-20 16:33:26 -0300

    Adding support to BMP.

--- SDL2_image/Makefile_sdl2_image.mk
+++ SDL2_image/Makefile_sdl2_image.mk
@@ -8,10 +8,10 @@ N64_CXXFLAGS += -I$(SDL2_IMAGE_ROOT_DIR)/include
 N64_CFLAGS += -DSPNG_USE_MINIZ -DMINIZ_NO_TIME -D__STRICT_ANSI__ -DSPNG_DISABLE_OPT
 
 # supported files
-# N64_CFLAGS += -DLOAD_BMP
+N64_CFLAGS += -DLOAD_BMP
 N64_CFLAGS += -DLOAD_SPRITE
 
 # warnings from original code
-# N64_CFLAGS += -Wno-error=maybe-uninitialized -Wno-error=use-after-free
+N64_CFLAGS += -Wno-error=unused-but-set-variable -Wno-error=unused-variable
 
 C_SDL_IMAGE_FILES := $(wildcard ${SDL2_IMAGE_ROOT_DIR}/src/*.c)
